b0fcce2398d702e0f6302ce960bc9eab25360790
ayush112-gif <ayushshuklabd766@gmail.com>
2026-06-25 18:47:41 +0530

    fix cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
+
 const emailRoutes =
 require("./routes/email.routes");
 
@@ -16,7 +16,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(cors());
+
 
 console.log("Registering routes...");
 
